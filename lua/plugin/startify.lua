0beedcd5c4d237fc1e9dbee983e9e0db0e78f1fb
polamid23 <a.schaf@a-coding-project.de>
2023-07-13 11:37:45 +0200

    applied startify config

---
+++ lua/plugin/startify.lua
@@ -0,0 +1,6 @@
+vim.g.startify_session_persistence = 1
+vim.g.startify_change_to_dir = 0
+vim.g.startify_lists = {
+	{ type = 'sessions',  header = { '   Sessions' } },
+	{ type = 'bookmarks', header = { '   Bookmarks' } },
+}
