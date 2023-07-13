eebfbacab76f25a3e2e72c40f11a0024c84893e5
polamid23 <a.schaf@a-coding-project.de>
2023-07-13 11:32:08 +0200

    applied copilot config

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 vim.g.mapleader = " "
+vim.g.copilot_no_tab_map = true
 
 require('plugins')
 require('lsp')
---
+++ lua/plugin/copilot.lua
@@ -0,0 +1,2 @@
+vim.api.nvim_set_keymap('i', '<C-k>', 'copilot#Accept("<CR>")', {expr = true, silent = true})
+
