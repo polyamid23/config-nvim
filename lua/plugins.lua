455fdbeca50bec38046bcf4a0d1dd01998280a91
Andre Schaf <arne23@fedora.fritz.box>
2025-06-10 18:34:27 +0200

    added roslyn

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -60,15 +60,24 @@ require("lazy").setup({
         }
     },
     {
-      'stevearc/oil.nvim',
-      ---@module 'oil'
-      ---@type oil.SetupOpts
-      opts = {},
-      -- Optional dependencies
-      dependencies = { { "echasnovski/mini.icons", opts = {} } },
-      -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-      -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-      lazy = false,
+        "seblyng/roslyn.nvim",
+        ft = "cs",
+        -- @module 'roslyn.config'
+        -- @type RoslynNvimConfig
+        opts = {
+            -- your configuration comes here; leave empty for default settings
+        },
+    },
+    {
+        'stevearc/oil.nvim',
+        ---@module 'oil'
+        ---@type oil.SetupOpts
+        opts = {},
+        -- Optional dependencies
+        dependencies = { { "echasnovski/mini.icons", opts = {} } },
+        -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+        -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+        lazy = false,
     },
     {
         "ThePrimeagen/harpoon",
